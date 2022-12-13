72638f8c3048ec611aad79a1d59900dd91f20cce
Anna-Urzhumtseva <63893421+Anna-Urzhumtseva@users.noreply.github.com>
2022-12-13 15:25:55 +0400

    Create Worm.cs

---
+++ Worm.cs
@@ -0,0 +1,38 @@
+namespace Animals
+{
+    public class Worm : Animal
+    {
+        private string Name { get; set; }
+        public Worm(string name, int age) : base(age, TypeOfFood.Else)
+        {
+            this.Name = name;
+        }
+
+        public override string About()
+        {
+            string comment;
+            if(Age >= 10)
+            {
+                comment = "они столько не живут =)";
+            } else if(Age <= 5)
+            {
+                comment = "ПРиемлемо";
+            } else
+            {
+                comment = "Червячок - старичок";
+            }
+
+            return $"Name: {Name} \n Age: {Age} {comment} \n Type: Else";
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Кушою и копаю");
+        }
+
+        public override void Move()
+        {
+            Console.WriteLine("Ползу");
+        }
+    }
+}
